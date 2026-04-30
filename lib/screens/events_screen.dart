3ec88594add5b529bbb2bcd7a0b977ae521ae602
SAI VISHNU VARDHAN PENAGENTI <126770083+vardhanv759@users.noreply.github.com>
2026-04-30 01:30:20 +0100

    new changes

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -1,5 +1,3 @@
-// lib/screens/events_screen.dart
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -279,7 +277,7 @@ class _EventsListScreenState extends State<EventsListScreen>
                                 );
                               },
                             ),
-                            const SizedBox(height: 32),
+                            const SizedBox(height: 26),
                           ],
                         );
                       }
